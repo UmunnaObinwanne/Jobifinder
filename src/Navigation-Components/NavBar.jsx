40335298639aeaa4b77c30d0baadca0448d53057
Mike <broadwaymarketingconsults@gmail.com>
2024-05-28 22:28:18 +0200

    fixed sign-out logic

--- src/Navigation-Components/NavBar.jsx
+++ src/Navigation-Components/NavBar.jsx
@@ -40,7 +40,7 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="bg-green-100 p-4 shadow-md">
+      <nav className="bg-green-100 p-12 shadow-md mb-5">
         <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
           <div className="text-teal-500 font-bold text-3xl mb-4 lg:mb-0 hover:text-green-600 hover:cursor-pointer">
             <Link to="/">Jobifinder</Link>
@@ -112,7 +112,7 @@ export default function Navbar() {
                     <path d="M13.962,8.885l-3.736,3.739c-0.086,0.086-0.201,0.13-0.314,0.13S9.686,12.71,9.6,12.624l-3.562-3.56C5.863,8.892,5.863,8.611,6.036,8.438c0.175-0.173,0.454-0.173,0.626,0l3.25,3.247l3.426-3.424c0.173-0.172,0.451-0.172,0.624,0C14.137,8.434,14.137,8.712,13.962,8.885 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.148,17.521,17.521,14.147,17.521,10"></path>
                   </svg>
                   {isDropdownOpen && (
-                    <ul className="menu dropdown-content  rounded-box w-52 mt-4 absolute right-0">
+                    <ul className="menu dropdown-content  rounded-box w-52 mt-2 p-1 absolute right-0">
                       <li>
                         <button onClick={handleLogout}>Sign Out</button>
                       </li>
